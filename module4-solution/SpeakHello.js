6bfd6fd95130410927f88a82381b8be657f85c2e
Arda <aerlik20@ku.edu.tr>
2021-07-07 15:10:27 +0300

    mod4 sol

---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,9 @@
+(function (window) {
+  var helloSpeaker = {};
+  var speakWord = "Hello";
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
